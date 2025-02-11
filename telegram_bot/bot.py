4acfbbe43db3991baadcfdc459f3ab15004decc1
Pritam Kumar <97377005+anupam535@users.noreply.github.com>
2025-02-11 18:36:15 +0530

    Create bot.py

---
+++ telegram_bot/bot.py
@@ -0,0 +1,22 @@
+import telebot
+from core.tts_engine import TTSEngine
+from utils.config import Config
+import os
+
+bot = telebot.TeleBot(Config.TELEGRAM_BOT_TOKEN)
+tts_engine = TTSEngine()
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to Hindi TTS Bot! Send me some text to convert it to speech.")
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    text = message.text
+    audio_path = tts_engine.generate_speech(text, output_format="mp3")
+
+    with open(audio_path, 'rb') as audio_file:
+        bot.send_voice(message.chat.id, audio_file)
+
+if __name__ == '__main__':
+    bot.polling()
