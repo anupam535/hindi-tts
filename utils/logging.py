9efee98b0e686417d9876de721f99bb0b1fd9ea7
Pritam Kumar <97377005+anupam535@users.noreply.github.com>
2025-02-11 18:50:48 +0530

    Create logging.py

---
+++ utils/logging.py
@@ -0,0 +1,15 @@
+import logging
+from utils.config import Config
+
+# Configure logging
+logging.basicConfig(
+    filename='app.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def log_info(message):
+    logging.info(message)
+
+def log_error(message):
+    logging.error(message)
